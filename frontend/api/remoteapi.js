856ee957e04196a0adba787bf00dfb5691927d36
Paul Johnson <Paul@PJOHNSOMAC-6366.local>
2024-02-25 13:49:45 -0500

    http

--- frontend/api/remoteapi.js
+++ frontend/api/remoteapi.js
@@ -1,27 +1,33 @@
-const httpProxy = require('http-proxy');
+const httpProxy = require("http-proxy");
 
 // Create a proxy server with custom application logic
 const proxy = httpProxy.createProxyServer({});
 
 module.exports = (req, res) => {
   // Extract the target URL from the header
-  const target = req.headers['x-remote-api'];
+  const target = req.headers["x-remote-api"];
 
   if (!target) {
-    res.writeHead(400, { 'Content-Type': 'text/plain' });
-    return res.end('Error: Missing X-Remote-Api header');
+    res.writeHead(400, { "Content-Type": "text/plain" });
+    return res.end("Error: Missing X-Remote-Api header");
   }
 
   // Optionally, manipulate the request URL or headers here
-  req.url = req.url.replace(/^\/remote-api/, ''); // Adjust the URL
-  
+  req.url = req.url.replace(/^\/remote-api/, ""); // Adjust the URL
+
   // Remove headers that might interfere with the proxy
   delete req.headers.host;
+  const httpTarget = target.startsWith("http://") ? target : `http://${target}`;
 
   // Proxy the request
-  proxy.web(req, res, { target, changeOrigin: true }, error => {
-    console.error('Proxy error:', error);
-    res.writeHead(500, { 'Content-Type': 'text/plain' });
-    res.text(`Proxy error: ${error.message}, ${target}`);
-  });
+  proxy.web(
+    req,
+    res,
+    { target: httpTarget, changeOrigin: true, secure: false },
+    (error) => {
+      console.error("Proxy error:", error);
+      res.writeHead(500, { "Content-Type": "text/plain" });
+      res.text(`Proxy error: ${error.message}, ${target}`);
+    }
+  );
 };
